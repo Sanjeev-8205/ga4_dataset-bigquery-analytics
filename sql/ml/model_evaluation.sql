754084fbdcd23a8def5209d83b62b6bcd7588fd5
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-01-30 17:21:44 +0530

    Create model_evaluation.sql

---
+++ sql/ml/model_evaluation.sql
@@ -0,0 +1,24 @@
+--model_evaluation.sql
+--Purpose: To evaluate the model based on different metrics.
+--Note: Evaluate the model using the December month dates instead of November.
+
+SELECT
+  *
+FROM ML.EVALUATE(
+  MODEL `sunny-web-483615-q0.ML.channel_conversion_model`,
+  (
+    SELECT
+      channel,
+      session_users,
+      purchases,
+      revenue,
+      revenue_7d,
+      revenue_14d,
+      revenue_30d,
+      conversion_rate,
+      revenue_tomorrow,
+      revenue_up
+    FROM `sunny-web-483615-q0.ML.mart_channel_ml_features`
+    WHERE session_date>='2020-12-01'
+  )
+);
