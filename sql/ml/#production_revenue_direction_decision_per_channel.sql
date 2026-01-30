20c6f8611dcc2a02a5c704a9704a8e0d025dbc74
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-01-30 18:45:02 +0530

    Create #production_revenue_direction_decision_per_channel.sql

---
+++ sql/ml/#production_revenue_direction_decision_per_channel.sql
@@ -0,0 +1,23 @@
+--#production_revenue_direction_decision_per_channel.sql
+--Purpose: To calculate the different confidence predictions for each channel
+--Source: ML.backtest_predictions
+
+SELECT
+  channel,
+  buckets,
+  COUNT(*) AS total_cases,
+  AVG(revenue_up) AS avg_revenue_up
+FROM(
+  SELECT
+    '2020-12-15',
+    channel,
+    revenue_up,
+    CASE
+      WHEN forecasted_revenue_up>=0.7 THEN 'HIGH'
+      WHEN forecasted_Revenue_up>=0.6 THEN 'MEDIUM'
+      ELSE 'LOW'
+    END AS buckets,
+  FROM sunny-web-483615-q0.ML.backtest_predictions
+)
+GROUP BY channel,buckets
+ORDER BY channel,buckets
