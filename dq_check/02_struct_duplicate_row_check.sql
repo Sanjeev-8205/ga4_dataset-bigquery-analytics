533f2174861cac65694c2b0484ac47266a15783b
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-04 02:12:28 +0530

    Update and rename 2_stg_events_duplicate_check.sql to 02_struct_duplicate_row_check.sql

---
+++ dq_check/02_struct_duplicate_row_check.sql
@@ -0,0 +1,13 @@
+--02_struct_duplicate_row_check.sql
+
+--CONTRACT:
+--Rows should never be duplicate.
+
+--Table: stg.events
+--Critical_columns: session_key, event_ts, event_name
+
+SELECT
+  COUNT(*) AS total_rows,
+  COUNT(DISTINCT CONCAT(session_key,'-',event_ts,'-',event_name)) AS distinct_no_of_rows,
+  COUNT(*)-COUNT(DISTINCT CONCAT(session_key,'-',event_ts,'-',event_name)) AS duplicates
+FROM `sunny-web-483615-q0.stg.events`
